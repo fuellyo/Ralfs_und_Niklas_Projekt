dc7a1a2ee91f7e075c03e4419a93224a1b380b49
Niklas <niklasfuelling@gmail.com>
2019-12-24 00:19:55 +0100

    Passwort festgelegt und Activity hinzugefügt

--- app/src/main/java/com/example/beispielapp/MainActivity.java
+++ app/src/main/java/com/example/beispielapp/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.beispielapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btncheck);
+        final EditText et = findViewById(R.id.passfield);
+        final TextView tw = findViewById(R.id.textfield);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String password = et.getText().toString();
+                if(password.equals("pipi")){
+                    setContentView(R.layout.activity_start);
+                }else{
+                    tw.setText("Falsch!");
+
+                }
+            }
+        });
     }
 }
